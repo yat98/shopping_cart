e755c8be2059a1021f9bb4bc7d4a80d963e6818f
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-06-05 00:36:31 +0800

    add checkout

--- js/cart.js
+++ js/cart.js
@@ -5,6 +5,7 @@ const addCart = (e) => {
   const nama = card.getElementsByClassName('card-title')[0].innerText;
   const harga = card.getElementsByClassName('card-action')[0].innerText
   addItem(nama, harga);
+  updateTotal();
 }
 
 const addItem = (nama, harga) => {
@@ -22,7 +23,7 @@ const addItem = (nama, harga) => {
       <input disabled class="nama-naga" type="text" value="${nama}">
     </div>
     <div class="input-field col s3">
-      <input disabled class="harga" type="text" value="${hargaFormat}">
+      <input disabled class="harga" type="text" value="${hargaFormat}JT">
     </div>
     <div class="col s3">
       <label>Jumlah</label>
@@ -39,6 +40,11 @@ const addItem = (nama, harga) => {
   row.innerHTML = item;
   isiCart.append(row);
   M.toast({html: `${nama} ditambahkan ke cart`});
+
+  const jumlahEls = document.getElementsByClassName('browser-default');
+  for(let jumlahEl of jumlahEls){
+    jumlahEl.addEventListener('change', updateCart);
+  }
 }
 
 const updateCart = (e) => {
@@ -68,9 +74,32 @@ const updateTotal = () => {
   totalEl.value = `${convertToRupiah(total)}.000.000,-`;
 }
 
+const checkout = (e) => {
+  const el = e.target;
+  const parentEl = el.parentElement;
+  const total = parentEl.getElementsByClassName('total')[0];
+  const jumlahPembayaranEl = document.getElementById('jumlah-pembayaran');
+  jumlahPembayaranEl.innerText = `Rp.${total.value}`;
+  var elems = document.querySelectorAll('.modal');
+  M.Modal.init(elems);
+}
+
+const pay = () => {
+  const isiCart = document.getElementById('isiCart');
+  while(isiCart.hasChildNodes()){
+    isiCart.removeChild(isiCart.firstChild);
+  }
+
+  updateTotal();
+  const total = document.getElementsByClassName('total')[0];
+  total.value = 0;
+}
+
 const ready = () => {
   const jumlahEls = document.getElementsByClassName('browser-default');
   const addEls = document.getElementsByClassName('pulse');
+  const beliEl = document.getElementsByClassName('beli')[0];
+  const bayarEl = document.getElementsByClassName('bayar')[0];
 
   for(let jumlahEl of jumlahEls){
     jumlahEl.addEventListener('change', updateCart);
@@ -80,6 +109,8 @@ const ready = () => {
     addEl.addEventListener('click', addCart);
   }
 
+  beliEl.addEventListener('click', checkout);
+  bayarEl.addEventListener('click', pay);
 }
 
 if(document.readyState == 'loading'){
