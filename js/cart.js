6f376ffc11100ee50be76175a05f2bcba3da1a2d
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-06-04 23:00:46 +0800

    add iterm to cart

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,46 @@
+const addCart = (e) => {
+  e.preventDefault();
+  const el = e.target;
+  const card = el.parentElement.parentElement.parentElement;
+  const nama = card.getElementsByClassName('card-title')[0].innerText;
+  const harga = card.getElementsByClassName('card-action')[0].innerText
+  addItem(nama, harga);
+}
+
+const addItem = (nama, harga) => {
+  const isiCart = document.getElementById('isiCart');
+  const hargaFormat = parseFloat(harga.replace('Rp.',''));
+  const namaNagaEls  = document.getElementsByClassName('nama-naga');
+  for(let namaNagaEl of namaNagaEls){
+    if(namaNagaEl.value == nama){
+      M.toast({html: `${nama} ini sudah ada di cart`});
+      return;
+    }
+  }
+  const row = document.createElement('div');
+  const item = `<div class="input-field col s6">
+      <input disabled class="nama-naga" type="text" value="${nama}">
+    </div>
+    <div class="input-field col s3">
+      <input disabled class="harga" type="text" value="${hargaFormat}">
+    </div>
+    <div class="col s3">
+      <label>Jumlah</label>
+      <select class="browser-default">
+        <option value="0">0</option>
+        <option value="1" selected>1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </div>`;
+  row.classList.add('row');
+  row.innerHTML = item;
+  isiCart.append(row);
+  M.toast({html: `${nama} ditambahkan ke cart`});
+}
+
 const updateCart = (e) => {
   const el = e.target;
   let value = e.target.value;
@@ -27,10 +70,16 @@ const updateTotal = () => {
 
 const ready = () => {
   const jumlahEls = document.getElementsByClassName('browser-default');
+  const addEls = document.getElementsByClassName('pulse');
 
   for(let jumlahEl of jumlahEls){
     jumlahEl.addEventListener('change', updateCart);
   }
+
+  for(let addEl of addEls){
+    addEl.addEventListener('click', addCart);
+  }
+
 }
 
 if(document.readyState == 'loading'){
